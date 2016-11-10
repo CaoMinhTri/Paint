6f74f1b214633cb22e50df85600f647f7ac53e1f
TriCaoMinh <tricm92@gmail.com>
2016-11-10 16:20:25 +0700

    Add files via upload

---
+++ DesignPattern/src/model/ShapeFactory.java
@@ -0,0 +1,13 @@
+package model;
+
+public class ShapeFactory {
+	public static Shape getShape(String shapeType){
+		if(shapeType.equalsIgnoreCase("CIRCLE")){
+			return new Circle();
+		}else if(shapeType.equalsIgnoreCase("RECTANGLE")){
+			return new Rectangle();
+		}else{
+			return null;
+		}
+	}
+}
